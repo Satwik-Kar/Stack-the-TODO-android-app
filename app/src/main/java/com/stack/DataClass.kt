09f5e888c61fd2e0f9029f91796ca80d8e70dd25
Satwik Kar <satwik.k.2000@gmail.com>
2024-08-05 11:23:18 +0530

    added new val to data class note

--- app/src/main/java/com/stack/DataClass.kt
+++ app/src/main/java/com/stack/DataClass.kt
@@ -5,5 +5,5 @@ import java.util.Date
 class Todo(val title: String,val description: String, val dateTime:Date) {
 }
 
-class Note(val title: String,val content: String) {
+class Note(val id:Int, val title: String,val content: String,val timeStamp:String) {
 }
