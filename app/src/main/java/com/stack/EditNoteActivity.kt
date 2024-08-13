c526f9581ceb64c0f6d4dbfd39436656a9ba6b45
Satwik Kar <satwik.k.2000@gmail.com>
2024-08-13 13:38:32 +0530

    updated lifecycle methods.for note activity to save note at any case

--- app/src/main/java/com/stack/EditNoteActivity.kt
+++ app/src/main/java/com/stack/EditNoteActivity.kt
@@ -1,27 +1,49 @@
 package com.stack
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class EditNoteActivity : AppCompatActivity() {
-    lateinit var toolbar: Toolbar
+    private lateinit var toolbar: Toolbar
+    private lateinit var backBtn: FloatingActionButton
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveNote()
+        finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_edit_note)
+        backBtn = findViewById(R.id.backBtn)
         toolbar = findViewById(R.id.note_toolbar)
         toolbar.title = ""
 
         setSupportActionBar(toolbar)
+        backBtn.setOnClickListener {
+
+            saveNote()
+            finish()
+        }
         onBackPressedDispatcher.addCallback(this) {
             saveNote()
 
             finish()
         }
+
     }
 
 
