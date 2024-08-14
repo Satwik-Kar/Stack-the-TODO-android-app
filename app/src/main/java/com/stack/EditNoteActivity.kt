da264f16fa700138818d262ab7366ba26f1c92cf
Satwik Kar <satwik.k.2000@gmail.com>
2024-08-14 11:19:21 +0530

    added edit functionality

--- app/src/main/java/com/stack/EditNoteActivity.kt
+++ app/src/main/java/com/stack/EditNoteActivity.kt
@@ -1,6 +1,7 @@
 package com.stack
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -8,10 +9,17 @@ import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.properties.Delegates
 
 class EditNoteActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var backBtn: FloatingActionButton
+    private var noteId by Delegates.notNull<Int>()
+    private var noteContent: String? = null
+    private var noteTitle: String? = null
+    private var noteTimeStamp: String? = null
+    private lateinit var editTitle: EditText
+    private lateinit var editNote: EditText
 
     override fun onDestroy() {
         super.onDestroy()
@@ -26,13 +34,24 @@ class EditNoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_edit_note)
+
         backBtn = findViewById(R.id.backBtn)
         toolbar = findViewById(R.id.note_toolbar)
         toolbar.title = ""
+        noteId = intent.getIntExtra("note_id", -1)
+        noteContent = intent.getStringExtra("note_content")
+        noteTitle = intent.getStringExtra("note_title")
+        noteTimeStamp = intent.getStringExtra("note_timestamp")
+
+        Log.e("debug", "onCreate: $noteTitle $noteContent")
+        editTitle = findViewById(R.id.edit_title)
+        editNote = findViewById(R.id.edit_note)
 
         setSupportActionBar(toolbar)
+        editTitle.setText(noteTitle)
+        editNote.setText(noteContent)
+
         backBtn.setOnClickListener {
 
             saveNote()
@@ -50,10 +69,16 @@ class EditNoteActivity : AppCompatActivity() {
     private fun saveNote(): Unit {
         val noteTitle = findViewById<EditText>(R.id.edit_title)
         val noteContent = findViewById<EditText>(R.id.edit_note)
-        if (noteTitle.text.isNotEmpty() || noteContent.text.isNotEmpty()) {
+        if ((noteTitle.text != null || noteContent.text != null) && (noteId != -1)) {
             val database = DatabaseHelper(this@EditNoteActivity)
             DatabaseHelper.setUserLoggedIn(true)
-            database.addNote(noteTitle.text.toString(), noteContent.text.toString())
+
+            if (intent.getBooleanExtra("editable", false)) {
+                database.updateNote(noteId, noteTitle.text.toString(), noteContent.text.toString())
+            } else {
+                database.addNote(noteTitle.text.toString(), noteContent.text.toString())
+            }
+
         }
     }
 
